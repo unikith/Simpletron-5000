3e5c3405f7862180188559f693d3f740ae49f8ad
unikith <james@thewholefamily.org>
2017-04-14 17:55:49 -0700

    Added documentation

--- Simpletron 5000/main.cpp
+++ Simpletron 5000/main.cpp
@@ -1,11 +1,18 @@
-#include "ComputerSimulator.h"
+///////////////////////////////////////////////////////////////////
+/// Programmer : James Nelson
+/// Project : Build a complier for a restricted form of basic, 
+///				for a simulated computer called simpletron.
+/// Note : From "C++ How to Program" by Dietel and Deitel
+///			problem from sec. 8 and 19
+///////////////////////////////////////////////////////////////////
 
+#include "ComputerSimulator.h"
 
 int main(void)
 {
 
-	ComputerSimulator PC;
-	PC.runComputer();
+	ComputerSimulator Simpletron;
+	Simpletron.runComputer();
 
 	return 0;
 }
