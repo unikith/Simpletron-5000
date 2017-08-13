74384527a56717f315b938fab12cb130e738c726
unikith <james@thewholefamily.org>
2017-08-13 15:22:48 -0700

    Added infix to postfix convertion to the compiler

--- Simpletron 5000/Compiler.cpp
+++ Simpletron 5000/Compiler.cpp
@@ -23,7 +23,106 @@ int Compiler::evalutateExpression(string & expression)
 // and turns it into a postfix expression in string format
 string Compiler::infixToPostfix(string  & infixExpresssion)
 {
+	string postfix = "";
+	stack<char> theStack;
+
+	theStack.push('(');
+	infixExpresssion.append(")");
+	int infixCounter = 0;
+	char currentChar = '\0';
+
+	while (!theStack.empty())
+	{
+		currentChar = infixExpresssion[infixCounter];
+		if (isdigit(currentChar))
+		{
+			postfix.append(string(1, currentChar));
+		}
+		else if (currentChar == '(')
+		{
+			theStack.push(currentChar);
+		}
+		else if (isOperator(currentChar))
+		{
+			while (precedenceExpression(theStack.top(), currentChar))
+			{
+				postfix.append(string(1, theStack.top()));
+				theStack.pop();
+			}
+			postfix.append(string(1, currentChar));
+		}
+		else if(currentChar == ')')
+		{
+			while (theStack.top() != '(')
+			{
+				postfix.append(string(1, theStack.top()));
+				theStack.pop();
+			}
+			theStack.pop();
+		}
+		infixCounter++;
+	}
+
 	return string();
+}
+
+bool Compiler::isOperator(char toCheck)
+{
+	// utilizes fall through
+	switch (toCheck)
+	{ 
+	case '+':
+	case '-':
+	case '*':
+	case '/':
+	case '%':
+	case '^':
+		return true;
+	default:
+		return false;
+	}
+}
+
+bool Compiler::precedenceExpression(char op1, char op2)
+{
+	int pres1 = 0, pres2 = 0;
+	// useses fall through and gives an int value to the presedence
+	// tiers
+	switch (op1)
+	{
+	case '+':
+	case '-':
+		pres1 = 1;
+	case '*':
+	case '/':
+	case '%':
+		pres1 = 2;
+	default:
+		pres1 = 0;
+		break;
+	}
+
+	switch (op2)
+	{
+	case '+':
+	case '-':
+		pres2 = 1;
+	case '*':
+	case '/':
+	case '%':
+		pres2 = 2;
+	default:
+		pres2 = 0;
+		break;
+	}
+	if (pres1 >= pres2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 
--- Simpletron 5000/Compiler.h
+++ Simpletron 5000/Compiler.h
@@ -1,10 +1,10 @@
 #pragma once
 
-
+#include <stack>
 #include <string>
 
+using std::stack;
 using std::string;
-
 
 class Compiler
 {
@@ -16,9 +16,20 @@ public:
 	int evalutateExpression(string & expression);
 
 private:
-	// This fucntion takes an infix expression in string format and 
+	// takes an infix expression in string format and 
 	// turns it into a postfix expression in string format
 	string infixToPostfix(string  & infixExpresssion);
+
+	// returns whether the input char is an operator or not
+	// Note: operators it checks for are expression valid ones
+	bool isOperator(char toCheck);
+
+	// retruns true if op1 has a greater than or equal to presedence  of op2 
+	// and false otherwise
+	// PRECON: only use with valid operators
+	bool precedenceExpression(char op1, char op2);
+
+
 
 	// evaluates a postfix expression down to a single integer
 	int evaluatePostfix(string & expression);
